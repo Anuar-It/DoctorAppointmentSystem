a9eae489e39870b16d9a69e28ca6272ac30657e0
Anuar-It <ryzenamude@gmail.com>
2026-02-04 22:20:34 +0500

    Delete src/main directory

--- src/main/java/com/company/Main.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-import com.company.controller.ConsoleController;
-
-public class Main {
-    public static void main(String[] args) {
-        new ConsoleController().start();
-    }
-}
--- src/main/java/com/company/entity/Doctor.java
+++
@@ -1,7 +0,0 @@
-package com.company.entity;
-
-public class Doctor {
-    private int id;
-    private String name;
-    private String specialization;
-}

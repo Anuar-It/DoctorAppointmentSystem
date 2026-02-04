6e7f5ff230e09e32935b1408e3d51c2ddedb7b8c
aruzhanomarova <aruzhanomarova12@gmail.com>
2026-02-04 07:55:07 +0500

    Update Session.java

--- src/main/java/com/company/session/Session.java
+++ src/main/java/com/company/session/Session.java
@@ -4,7 +4,6 @@ import com.company.entity.User;
 
 public class Session {
     private User currentUser;
-
     public User getCurrentUser() { return currentUser; }
     public void setCurrentUser(User u) { this.currentUser = u; }
     public void clear() { this.currentUser = null; }
